9c8e3af78608761865c4e36e4a400353dcde1a6c
0xCoto <cotothearcher@gmail.com>
2020-06-14 20:46:16 +0300

    Update simpleReader.py

--- simpleReader.py
+++ simpleReader.py
@@ -1,5 +1,5 @@
 # A simple program to read and plot pulsar data files
-# The program uses a manually entered period and phase offset, rather than TEMPO.
+# The program uses a manually entered period and phase offset, rather than tempo.
 # It does not accommodate the possibilty of of reading a file while it is
 # being written.
 
@@ -36,6 +36,8 @@ period = float(period)
 # Define the number of phase bins and frequencies
 DM, nBins, nFreqs = float(dm), int(nbins), 512
 
+def shift_col_up(phase_num,n_rows):
+    sumArray[:,phase_num] = np.roll(sumArray[:,phase_num], -n_rows)
 def estimate_S_N(averagePhase,mask=np.array([])):
     """Simple estimation of signal_to_noise, with optional masking.
     If mask not given, then all channels will be used in estimating noise
@@ -121,20 +123,45 @@ while True:
 for i in range(nBins):
     sumArray[i] /= countsArray[i]
 
-# Compute the average signal for each frequency bin
+# Compute the average signal for each frequency bin (zero DM)
 averageFFT = np.average(sumArray,axis=0)
+
+# Fold data (zero DM)
+no_dm_averagePhase = np.nanmean(sumArray,axis=1)
 
 # Plot averageFFT
 freqs = np.linspace(1275.,1525.,len(averageFFT))
+
+# Mask channels wiht RFI
+#for i in range(sumArray.shape[1]):
+#    if np.mean(sumArray[:, i]) < 0.3:
+#        sumArray[:, i] = np.nan
+
+
+# Perform de-dispersion
+deltaF = float(np.max(freqs)-np.min(freqs))/sumArray.shape[1]
+f_start = np.min(freqs)
+for phase_num in range(sumArray.shape[1]):
+    f_chan = f_start+(phase_num)*deltaF
+    deltaT = 4149*dm*((1/(f_chan**2))-(1/(np.max(freqs)**2)))
+    n = int((float(deltaT)/(float(1)/sumArray.shape[0])))
+    shift_col_up(phase_num,n)
+
+# Compute the average signal for each frequency bin
+averageFFT = np.average(sumArray,axis=0)
 
 # Subtract the averaged spectrum from all rows
 for i in range(nBins):
     sumArray[i] -= averageFFT
 
-averagePhase = np.average(sumArray,axis=1)
+# Fold data (de-dispersed)
+averagePhase = np.nanmean(sumArray,axis=1)
 
 # Get the index of the pulse (i.e. global maximum)
 max_index = np.argmax(averagePhase)
+
+# Same for zero DM
+no_dm_max_index = np.argmax(no_dm_averagePhase)
 
 fig = plt.figure(figsize=(20,15))
 gs = gridspec.GridSpec(nrows=2, ncols=2, width_ratios=[1, 1.8], height_ratios=[1.5, 3])
@@ -146,6 +173,8 @@ gs = gridspec.GridSpec(nrows=2, ncols=2, width_ratios=[1, 1.8], height_ratios=[1
 ax3 = fig.add_subplot(gs[1, 1])
 ax3.plot(freqs, averageFFT, '#3182bd', linewidth=2.5)
 ax3.fill_between(freqs, averageFFT, color='#deebf7')
+#ax3.plot(freqs, averageFFT, '#bd3131', linewidth=2.5)
+#ax3.fill_between(freqs, averageFFT, color='#f7dede')
 ax3.set_xlim(np.min(freqs),np.max(freqs))
 ax3.set_title('Averaged Spectrum')
 ax3.set_xlabel('Frequency (MHz)')
@@ -162,8 +191,17 @@ mask = np.zeros_like(averagePhase)
 for i in range(max_index-int(nBins/10),max_index+int(nBins/10)):
     mask[i] = 1
 
-# Get the value of the S/N of the pulse
+no_dm_mask = np.zeros_like(no_dm_averagePhase)
+for i in range(no_dm_max_index-int(nBins/10),no_dm_max_index+int(nBins/10)):
+    no_dm_mask[i] = 1
+
+# Get the value of the S/N of the pulse and append it to file
 max_snr = np.amax(estimate_S_N(averagePhase, mask))
+
+with open('results.txt', 'a') as myfile:
+    myfile.write(str(nBins)+','+str(dm)+','+str(period)+','+str(max_snr)+'\n')
+
+no_dm_max_snr = np.amax(estimate_S_N(no_dm_averagePhase, no_dm_mask))
 
 # Rotate sumArray by 90 deg to match Phase axis values
 sumArray = np.rot90(sumArray)
@@ -184,12 +222,13 @@ ax2.set_ylabel('Frequency (MHz)')
 #cbar.ax.tick_params(size=0)
 #cbar.set_ticks([])
 
-
 # Plot Pulse Profile
 ax1 = fig.add_subplot(gs[0, 0])
 ax1.axvline(x=max_index, color='brown', linestyle='--', linewidth=2)
-ax1.plot(estimate_S_N(averagePhase, mask),'ro')
-ax1.annotate('S/N = '+str(max_snr)[:4], xy=(8, 316), xycoords='axes points', size=18, ha='left', va='top', color='brown')
+ax1.plot(estimate_S_N(averagePhase, mask),'ro-')
+ax1.plot(estimate_S_N(no_dm_averagePhase, mask),'go-')
+ax1.annotate('S/N (de-dispersed) = '+str('{:.2f}'.format(max_snr)), xy=(8, 316), xycoords='axes points', size=18, ha='left', va='top', color='brown')
+ax1.annotate('S/N (zero DM) = '+str('{:.2f}'.format(no_dm_max_snr)), xy=(8, 295), xycoords='axes points', size=18, ha='left', va='top', color='darkgreen')
 ax1.set_title('Pulse Profile')
 ax1.set_ylabel('Signal-to-Noise Ratio (S/N)')
 ax1.grid()
